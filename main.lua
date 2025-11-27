01370c9598e1dc3b8484d17d45c1f5dc2b128b6e
hussaien <hussaien@hussaien>
2025-11-27 19:46:39 +0300

    working on a simple damage mechanism

--- main.lua
+++ main.lua
@@ -5,6 +5,8 @@ function love.load()
  world = wf.newWorld(0, 0)
 
  world:addCollisionClass('plr')
+ world:addCollisionClass('proj')
+ world:addCollisionClass('dummy')
 
  world:setQueryDebugDrawing(true)  
 ----------------------------------- 
@@ -18,10 +20,17 @@ plr.speed = 200
 plr.collider = world:newBSGRectangleCollider(200, 200, 40, 50, 10)
 plr.collider:setFixedRotation(true)
 plr.collider:setCollisionClass('plr')
-
+plr.dir = "left"
 -----------------------------------
 
-
+--enemy properties-------------------------------------------
+enemy = {} 
+enemy.collider = world:newRectangleCollider(200, 200, 50, 50)
+enemy.x = enemy.collider:getX() 
+enemy.y = enemy.collider:getY() 
+enemy.collider:setType('static')
+enemy.collider:setCollisionClass('dummy')
+-------------------------------------------------------------
 
 end 
 
@@ -31,6 +40,9 @@ function love.update(dt)
 
 px, py = plr.collider:getPosition() 
 
+--so the enemy health is constantly updated duh--
+enemy.health = 60
+-------------------------------------------------
 
 --velocities for the player's collider-- 
  vx = 0
@@ -67,6 +79,51 @@ plr.y = plr.collider:getY()
 ----------------------------------------------------------------
 
 
+
+--projectile config--------------------------------------------------
+function shoot() 
+
+    proj = {}
+    proj.x = plr.x + 30  
+    proj.y = plr.y
+    proj.rad = 10 
+
+
+    --projectile direction----------------------
+    if plr.dir == "left" then 
+        proj.collider = world:newCircleCollider(proj.x - 60, proj.y, proj.rad)
+        proj.collider:setLinearVelocity(-300, 0)
+        proj.collider:setCollisionClass('proj')
+
+    elseif plr.dir == "right" then 
+        proj.collider = world:newCircleCollider(proj.x + 1, proj.y, proj.rad)
+        proj.collider:setLinearVelocity(300, 0)
+        proj.collider:setCollisionClass('proj')
+    end 
+
+    if plr.dir == "up" then 
+        proj.collider = world:newCircleCollider(proj.x - 30, proj.y - 30, proj.rad)
+        proj.collider:setLinearVelocity(0, -300)
+        proj.collider:setCollisionClass('proj')
+
+    elseif plr.dir == "down" then 
+        proj.collider = world:newCircleCollider(proj.x - 30, proj.y + 30, proj.rad)
+        proj.collider:setLinearVelocity(0, 300)
+        proj.collider:setCollisionClass('proj')
+    end 
+    --------------------------------------------- 
+
+    --simple collision detection for projectiles and enemies--
+    if proj.collider:enter('dummy') then 
+        enemy.collider:destroy()
+    end  
+
+
+    -----------------------------------------------------------
+end
+--------------------------------------------------------------------- 
+
+
 end 
 
 
@@ -81,8 +138,11 @@ world:draw()
 end 
 
 
+
 function love.keypressed(key)
 
-
+ if key == 'space' then 
+    shoot() 
+ end 
 
 end
